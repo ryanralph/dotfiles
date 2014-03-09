07f52a9c1dc90cc2594a9d377a78e459fcff5104
Ryan Ralph <ryan@ryanralph.net>
2014-03-09 22:34:55 +1100

    Added symbolic links

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ln -s ~/dotfiles/.bash_aliases ~/
 ln -s ~/dotfiles/.bashrc ~/
+ln -s ~/dotfiles/.git ~/
 ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.vim ~/
 ln -s ~/dotfiles/.vimrc ~/
-ln -s ~/dotfiles/.vim/ ~/
+ln -s ~/dotfiles/.xinitrc ~/
